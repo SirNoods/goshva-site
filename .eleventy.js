6377efbe23a66e43c1b8c2fbcb7394981f26ba56
Goshva <42236284+SirNoods@users.noreply.github.com>
2025-11-01 19:35:53 +0100

    fixed video playback

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
 eleventyConfig.addPassthroughCopy("./src/css");
 eleventyConfig.addWatchTarget("./src/css/");
+eleventyConfig.addPassthroughCopy("./src/assets");
   return {
     dir: {
       input: "src",
